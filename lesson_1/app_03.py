5d6ee9e80705d3005673cc6aa021e87429a1ed81
Elena <keaya@rambler.ru>
2023-08-04 18:05:08 +0300

    лекция1

---
+++ lesson_1/app_03.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Привет, незнакомец!'
+
+
+@app.route('/Николай/')
+def nike():
+    return 'Привет, Ниеолай!'
+
+
+@app.route('/Иван/')
+def ivan():
+    return 'Привет, Ванечка!'
+
+
+@app.route('/Фёдор/')
+@app.route('/Fedor/')
+@app.route('/Федя/')
+def fedor():
+    return 'Привет, Федор!'
+
+
+if __name__ == "__main__":
+    app.run()
